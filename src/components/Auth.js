05d86bdf36a9763d3c76ede2a651d80fd31a0e64
WoKrishhnaHai <krishnagupta4371@gmail.com>
2023-11-27 14:40:29 +0530

    short changes

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -16,29 +16,29 @@ function Auth() {
 
  return (
    <>
-      <h2 class="heading">Create New Customer Account</h2>
-      <div class="container">
-         <div class="signup-form">
+      <h2 className="text-center my-4">Create New Customer Account</h2>
+      <div className="container">
+         <div className="signup-form">
             <form action="/examples/actions/confirmation.php" method="post">
     
                <h5>Personal Information</h5>
                <hr/>
-               <div class="form-group">
-                  <div class="row">
-                     <div class="col"><input type="text" class="form-control" name="first_name" placeholder="First Name" required="required"/></div>
-                     <div class="col"><input type="text" class="form-control" name="last_name" placeholder="Last Name" required="required"/></div>
+               <div className="form-group">
+                  <div className="row">
+                     <div className="col"><input type="text" className="form-control" name="first_name" placeholder="First Name" required="required"/></div>
+                     <div className="col"><input type="text" className="form-control" name="last_name" placeholder="Last Name" required="required"/></div>
                   </div>        	
                </div>
-               <div class="form-check">
-                  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked/>
-                  <label class="form-check-label" for="flexCheckChecked">
+               <div className="form-check">
+                  <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked/>
+                  <label className="form-check-label" for="flexCheckChecked">
                   Sign Up for Newsletter
                   </label>
                </div>
                <br/>
                <h5>Address Information</h5>
                <hr/>
-               <div class="col"><input type="text" class="form-control" name="companny" placeholder="Company Name" required="required"/></div>
+               <div className="col"><input type="text" className="form-control" name="companny" placeholder="Company Name" required="required"/></div>
                <br/>
                <input
                      type="tel"
@@ -49,24 +49,24 @@ function Auth() {
                      title="Ten digits code"
                      required
                      />
-                  <label class="telephone">
+                  <label className="telephone">
                      Eg : 081 222 2224
                   </label>
                
                <br/>
                <br/>
-               <div class="form-floating mb-3">
-               <input type="email" class="form-control" id="floatingInput" placeholder="Street Address"/>
+               <div className="form-floating mb-3">
+               <input type="email" className="form-control" id="floatingInput" placeholder="Street Address"/>
                <label for="floatingInput">Street Address</label>
                </div>
                <br/>
-               <div class="form-floating mb-3">
-               <input type="email" class="form-control" id="floatingInput" placeholder="City"/>
+               <div className="form-floating mb-3">
+               <input type="email" className="form-control" id="floatingInput" placeholder="City"/>
                <label for="floatingInput">City</label>
                </div>
                <br/>
                
-               <select class="form-select" aria-label="Default select example">
+               <select className="form-select" aria-label="Default select example">
                <option selected>State/Province</option>
                <option value="1">Maharashtra</option>
                <option value="2">Jammu</option>
@@ -74,13 +74,13 @@ function Auth() {
                </select>
                <br/>
                <br/>
-               <div class="form-floating mb-3">
-               <input type="email" class="form-control" id="floatingInput" placeholder="Zip/Postal Code"/>
+               <div className="form-floating mb-3">
+               <input type="email" className="form-control" id="floatingInput" placeholder="Zip/Postal Code"/>
                <label for="floatingInput">Zip/Postal Code</label>
                </div>
                <br/>
               
-               <select class="form-select" aria-label="Default select example">
+               <select className="form-select" aria-label="Default select example">
                <option selected>Country</option>
                <option value="1">India</option>
                <option value="2">China</option>
@@ -92,24 +92,24 @@ function Auth() {
                <br/>
                <h5>Sign-in Information</h5>
                <hr/>
-               <div class="form-group">
-                  <input type="email" class="form-control" name="email" placeholder="Email" required="required"/>
+               <div className="form-group">
+                  <input type="email" className="form-control" name="email" placeholder="Email" required="required"/>
                </div>
-               <div class="form-group">
-                     <input type="password" class="form-control" name="password" placeholder="Password" required="required"/>
+               <div className="form-group">
+                     <input type="password" className="form-control" name="password" placeholder="Password" required="required"/>
                </div>
-               <div class="form-group">
-                     <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" required="required"/>
+               <div className="form-group">
+                     <input type="password" className="form-control" name="confirm_password" placeholder="Confirm Password" required="required"/>
                </div>     
                   
-               <div class="form-group">
-                  <label class="form-check-label"><input type="checkbox" required="required"/> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
+               <div className="form-group">
+                  <label className="form-check-label"><input type="checkbox" required="required"/> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
                </div>
-               <div class="form-group">
-                     <button type="submit" class="btn btn-success btn-lg btn-block">Register Now</button>
+               <div className="form-group">
+                     <button type="submit" className="btn btn-success btn-lg btn-block">Register Now</button>
                </div>
             </form>
-            <div class="text-center">Already have an account? <a href="#">Sign in</a></div>
+            <div className="text-center">Already have an account? <a href="#">Sign in</a></div>
          </div>
 </div>
  </>
